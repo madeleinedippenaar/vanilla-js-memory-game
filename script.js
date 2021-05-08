f93f7697df73866fbe5ab733081304ba80227bf5
madeleinedippenaar <madeleinedippenaar0@gmail.com>
2021-05-08 12:55:03 -0400

    added a little more logic for when cards match and set timeout for when they do not match

--- script.js
+++ script.js
@@ -1,7 +1,46 @@
+let firstCard;
+let SecondCard;
+let hasCardFlipped = false;
+
+
 const cards = document.querySelectorAll('.card-container');
 
+
+//function added to allow a toggle switch for a card clicked
 function flipCard() {
-    this.classList.toggle('flip');
+    // this.classList.toggle('flip');
+    this.classList.add('flip');
+
+    if(!hasCardFlipped) {
+        hasCardFlipped = true;
+        firstCard = this;
+        return;
+    }
+    SecondCard = this;
+    hasCardFlipped = false;
+
+    doCardsMatch();
+}
+
+//function to check if cards match with data-set added into html
+function doCardsMatch() {
+    if(firstCard.dataset.icon === SecondCard.dataset.icon) {
+        disable();
+        return;
+    }
+    unflippingCards();
+}
+
+function disable() {
+    firstCard.removeEventListener('click', flipCard);
+    SecondCard.removeEventListener('click', flipCard);
+}
+
+function unflippingCards() {
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        SecondCard.classList.remove('flip');
+    }, 2000);
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
